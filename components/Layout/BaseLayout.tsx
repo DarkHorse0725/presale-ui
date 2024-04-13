98057a6a64458baafb3a9a11d9ab8521d2f05158
SeniorDev <tech.eng.me@gmail.com>
2024-04-13 18:04:24 -0400

    I update seo head

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -6,7 +6,7 @@ import { ILayout } from "./types"
 const BaseLayout = ({ children }: ILayout) => (
   <div className="w-screen">
     <div className="w-full fixed top-0 left-0 z-[10]">
-      <SeoHead title="Presale" description="Presale UI" image="/images/Logo/logo.png" />
+      <SeoHead title="birdbyte" description="birdbyte UI" image="/images/SEO_LOGO_ICON.png" />
       <Header />
       {children}
     </div>
