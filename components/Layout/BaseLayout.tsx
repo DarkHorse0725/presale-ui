0cb5156143ca19c55243e09d61b6a18969b28641
SeniorDev <tech.eng.me@gmail.com>
2024-04-13 18:06:48 -0400

    I remove unused code

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -6,7 +6,7 @@ import { ILayout } from "./types"
 const BaseLayout = ({ children }: ILayout) => (
   <div className="w-screen">
     <div className="w-full fixed top-0 left-0 z-[10]">
-      <SeoHead title="birdbyte" description="birdbyte UI" image="/images/SEO_LOGO_ICON.png" />
+      <SeoHead title="birdbyte" description="birdbyte UI" image="/SEO_LOGO_ICON.png" />
       <Header />
       {children}
     </div>
