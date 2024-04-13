8fd2620d32e9a0b66d39d926c985d06e910847d8
seniorTech <tech.eng.me@gmail.com>
2024-04-13 19:48:37 -0400

    I update BaseLayout

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -5,7 +5,7 @@ import { ILayout } from "./types"
 
 const BaseLayout = ({ children }: ILayout) => (
   <div className="w-screen">
-    <div className="w-full fixed top-0 left-0 z-[10]">
+    <div className="w-full h-screen fixed top-0 left-0 z-[10] bg-[url('/images/screen_bg.png')] bg-cover">
       <SeoHead title="birdbyte" description="birdbyte UI" image="/SEO_LOGO_ICON.png" />
       <Header />
       {children}
