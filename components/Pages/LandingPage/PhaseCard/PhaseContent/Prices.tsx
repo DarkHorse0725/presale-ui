4e5112e5b783a7deac93fd27627addd6d325a5b8
seniorTech <tech.eng.me@gmail.com>
2024-04-14 08:43:28 -0400

    I add Prices and TargetBar

---
+++ components/Pages/LandingPage/PhaseCard/PhaseContent/Prices.tsx
@@ -0,0 +1,19 @@
+const Prices = () => (
+  <div className="grid grid-cols-2 gap-[20px] mt-[30px]">
+    <div className="flex items-center justify-center w-full border border-gray_5 rounded-[30px]">
+      <p className="font-poppins_medium text-gray_5 py-2 px-3 text-center text-[10px]">
+        Listing Price = $0.0700
+        <span className="text-green_3">(+700.00%)</span>
+      </p>
+    </div>
+
+    <div className="w-full border border-gray_5 rounded-[30px] flex items-center justify-center">
+      <p className="font-poppins_medium text-gray_5 py-2 px-3 text-center text-[10px]">
+        Listing Price = $0.0700
+        <span className="text-green_3">(+700.00%)</span>
+      </p>
+    </div>
+  </div>
+)
+
+export default Prices
