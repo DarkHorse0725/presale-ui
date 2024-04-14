43985df30c39406134c2496bc7c0d7eb325de17a
seniorTech <tech.eng.me@gmail.com>
2024-04-14 07:58:38 -0400

    I uploaded images

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,22 @@
+import Container from "@/components/Container"
 import Layout from "@/components/Layout"
+import PhaseFeature from "./PhaseFeature"
+import PhaseCard from "./PhaseCard"
 
-const LandingPage = () => <Layout type="base">this</Layout>
+const LandingPage = () => (
+  <Layout type="base">
+    <Container
+      containerClassName="flex mt-[100px]"
+      contentClassName="grid grid-cols-10 px-[10px] gap-[20px]"
+    >
+      <div className="col-span-6">
+        <PhaseCard />
+      </div>
+      <div className="col-span-4">
+        <PhaseFeature />
+      </div>
+    </Container>
+  </Layout>
+)
 
 export default LandingPage
