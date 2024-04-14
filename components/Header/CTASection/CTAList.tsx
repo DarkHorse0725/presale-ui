dd21c0c92a2cdee4e950eab75bc2a0d972782618
seniorTech <tech.eng.me@gmail.com>
2024-04-14 12:57:04 -0400

    I update font style

--- components/Header/CTASection/CTAList.tsx
+++ components/Header/CTASection/CTAList.tsx
@@ -4,13 +4,13 @@ const CTAList = () => (
   // eslint-disable
   <div className="flex gap-[20px]">
     <Link href="/whitepapper">
-      <div className="cursor-pointer font-Poppins text-[16px] text-gray_1">Whitepaper</div>
+      <div className="cursor-pointer font-poppins_medium text-[16px] text-gray_1">Whitepaper</div>
     </Link>
     <Link href="/tokennomics">
-      <div className="cursor-pointer font-Poppins text-[16px] text-gray_1">Tokenomics</div>
+      <div className="cursor-pointer font-poppins_medium text-[16px] text-gray_1">Tokenomics</div>
     </Link>
     <Link href="/tobuy">
-      <div className="cursor-pointer font-Poppins  text-[16px] text-gray_1">How to Buy</div>
+      <div className="cursor-pointer font-poppins_medium  text-[16px] text-gray_1">How to Buy</div>
     </Link>
   </div>
 )
