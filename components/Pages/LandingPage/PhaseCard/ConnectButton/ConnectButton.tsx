9c10fef7e25101b887a90918db3c91b8db984c15
seniorTech <tech.eng.me@gmail.com>
2024-04-14 09:13:54 -0400

    I create ConnectButton

---
+++ components/Pages/LandingPage/PhaseCard/ConnectButton/ConnectButton.tsx
@@ -0,0 +1,17 @@
+const ConnectButton = () => (
+  <div
+    className="w-full flex justify-center items-center 
+  rounded-[30px] p-3 border border-[#222222] 
+  mt-5 active:scale-[1.1]"
+  >
+    <button
+      className="w-full bg-pink_1 px-6 px-2 
+    text-white rounded-[20px]"
+      type="button"
+    >
+      Connect
+    </button>
+  </div>
+)
+
+export default ConnectButton
