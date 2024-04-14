058dadafe1b632bcf18242560f9c7aa1529df896
seniorTech <tech.eng.me@gmail.com>
2024-04-14 18:59:15 -0400

    I add buy now button

---
+++ components/Pages/LandingPage/PhaseCard/BuyButton/BuyButton.tsx
@@ -0,0 +1,19 @@
+const BuyButton = () => {
+    return (
+        <button
+        className="w-full flex justify-center items-center 
+        rounded-[30px] p-2 border border-black_2 
+        mt-4 transition duration-[300ms] hover:scale-[1.1]"
+        type="button"
+        >
+        <div
+            className="w-full bg-pink_1 py-2 px-6 
+        text-white rounded-[20px] font-poppins text-center"
+        >
+            Buy Now
+        </div>
+        </button>
+    )
+}
+
+export default BuyButton
--- components/Pages/LandingPage/PhaseCard/PhaseContent/PhaseContent.tsx
+++ components/Pages/LandingPage/PhaseCard/PhaseContent/PhaseContent.tsx
@@ -4,20 +4,29 @@ import BaseChains from "../BaseChains"
 import Amount from "../Amount"
 import Cost from "../Cost"
 import ConnectButton from "../ConnectButton"
+import WalletConnectButton from "@/components/WalletConnectButton"
+import { useEthersSigner } from "@/hooks/useEthersSigner"
+import BuyButton from "../BuyButton"
 
-const PhaseContent = () => (
-  <>
-    <TargetBar />
-    <Prices />
-    <div className="grid grid-cols-2 gap-x-[20px] gap-y-[10px] mt-[10px]">
-      <BaseChains />
-      <Amount />
-      <div className="col-span-2">
-        <Cost />
+const PhaseContent = () => {
+  const signer = useEthersSigner()
+
+  return (
+    <>
+      <TargetBar />
+      <Prices />
+      <div className="grid grid-cols-2 gap-x-[20px] gap-y-[10px] mt-[10px]">
+        <BaseChains />
+        <Amount />
+        <div className="col-span-2">
+          <Cost />
+        </div>
       </div>
-    </div>
-    <ConnectButton />
-  </>
-)
+      {signer ? <BuyButton /> : <WalletConnectButton>
+        <ConnectButton /> 
+      </WalletConnectButton>}
+    </>
+  )
+}
 
 export default PhaseContent
