dc1a23bdf8003ee2e4f3388e7d3078bfcc40bfec
seniorTech <tech.eng.me@gmail.com>
2024-04-14 07:49:26 -0400

    I update Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import CTASection from "./CTASection"
-import ChainNetwork from "./ChainNetwork"
 import Container from "../Container"
 
 const Header = () => (
@@ -9,7 +8,6 @@ const Header = () => (
     containerClassName="flex items-center justify-between"
   >
     <CTASection />
-    {/* <ChainNetwork /> */}
     <div className="flex items-center justify-center px-[10px] py-[10px] bg-gray_2 rounded-[20px]">
       <ConnectButton />
     </div>
