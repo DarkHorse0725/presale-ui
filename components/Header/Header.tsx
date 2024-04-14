e2bbfe8687d76399b214e4f469b80553ea84ddbb
seniorTech <tech.eng.me@gmail.com>
2024-04-13 23:22:42 -0400

    I update Header  section

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+import CTASection from "./CTASection"
+import ChainNetwork from "./ChainNetwork"
+import Container from "../Container"
 
 const Header = () => (
-  <div className="w-full py-2 px-2 flex justify-between">
-    <ConnectButton />
-  </div>
+  <Container
+    contentClassName="w-full flex justify-between py-[15px] px-[10px]"
+    containerClassName="flex items-center justify-between"
+  >
+    <CTASection />
+    {/* <ChainNetwork /> */}
+    <div className="flex items-center justify-center px-[10px] py-[10px] bg-gray_2 rounded-[20px]">
+      <ConnectButton />
+    </div>
+  </Container>
 )
 
 export default Header
---
+++ components/Header/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import Media from "@/shared/Media"
+
+const Logo = () => (
+  <div className="">
+    <Media
+      type="image"
+      link="/images/logo.svg"
+      blurLink="/images/logo.png"
+      containerClasses="w-[95px] aspect-[95/17]"
+    />
+  </div>
+)
+
+export default Logo
