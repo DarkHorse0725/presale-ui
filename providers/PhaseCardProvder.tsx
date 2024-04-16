1dfcd54c0d91f09949c795c84b56bf6f21c5e065
seniorTech <tech.eng.me@gmail.com>
2024-04-17 01:41:13 +0900

    I update PhaseCardProvider

--- providers/PhaseCardProvder.tsx
+++ providers/PhaseCardProvder.tsx
@@ -2,11 +2,12 @@ import useBaseChainAmount from "@/hooks/useBaseChainAmount"
 import useCostAmount from "@/hooks/useCostAmount"
 import { createContext, useMemo, useContext } from "react"
 
+
 const PhaseCardContext = createContext(null)
 
 const PhaseCardProvider = ({ children }) => {
   const baseChainData = useBaseChainAmount()
-  const costData = useCostAmount()
+  const costData = useCostAmount(baseChainData.baseAmount)
 
   const value = useMemo(
     () => ({
