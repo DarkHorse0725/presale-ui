d981cebd2baea7db298b2561c41d8863b474bd6a
seniorTech <tech.eng.me@gmail.com>
2024-04-18 12:20:17 +0900

    I fix seo head

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -8,7 +8,7 @@ const BaseLayout = ({ children }: ILayout) => (
       className="w-full top-0 left-0 z-[10]
                 bg-[url('/images/screen_bg.png')] bg-cover bg-no-repeat"
     >
-      <SeoHead title="birdbyte" description="birdbyte UI" image="/SEO_LOGO_ICON.png" />
+      <SeoHead title="Presale" description="Presale UI" image="/SEO_LOGO_ICON.png" />
       <Header />
       {children}
     </div>
